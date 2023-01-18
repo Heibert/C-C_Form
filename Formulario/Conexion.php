d4d6a9bab5de0207e9171df168b03e96ff397750
Sebas <carrenosebastian54@gmail.com>
2023-01-18 09:59:27 -0500

    Cambio a PHP - input y encabezado usuario

--- Formulario/Conexion.php
+++ Formulario/Conexion.php
@@ -7,6 +7,6 @@ $password = "Xy5t3m452023+-";
 $conn = mysqli_connect($servername, $username, $password, $database);
 // Check connection
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
 }
 mysqli_close($conn);
